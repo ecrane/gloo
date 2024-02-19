08aa8d9ac61204d4f9b088bf5d533006f0725eb0
Eric Crane <encrane@datacor.com>
2024-02-19 10:50:46 -0500

    fleshing out web server lifecycle with thread

--- lib/gloo/web_svr/doc.rb
+++ lib/gloo/web_svr/doc.rb
@@ -13,7 +13,21 @@ module Gloo
       #
       def self.run engine
         engine.log.info "Starting documentation web server…"
-        TestServer.start
+        engine.log.quiet = true
+
+        web_server = Server.new( engine )
+        web_server.start
+        
+        # Pause to give the server time to start.
+        sleep( 1 )
+
+        # Open the web page.
+        url = "http://localhost:8087/"
+        Gloo::Objs::Uri.open_url url
+
+        # Show prompt to quit
+        prompt = '<return> to quit the documentation web server >'
+        engine.platform.prompt.ask( prompt )
       end
 
     end
