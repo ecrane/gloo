6b9258ec0ca6c30c81ae7a5ecc8bc7744fee7e52
Eric Crane <encrane@datacor.com>
2024-04-08 07:48:09 -0400

    adds function obj

---
+++ lib/gloo/objs/basic/function.rb
@@ -0,0 +1,159 @@
+# Author::    Eric Crane  (mailto:eric.crane@mac.com)
+# Copyright:: Copyright (c) 2024 Eric Crane.  All rights reserved.
+#
+# A Function that can be invoked inline in a script.
+# It is also used as a helper for web pages.
+#
+
+module Gloo
+  module Objs
+    class Function < Gloo::Core::Obj
+
+      KEYWORD = 'function'.freeze
+      KEYWORD_SHORT = 'ƒ'.freeze
+
+      # Events
+      ON_INVOKE = 'on_invoke'.freeze
+
+      # Parameters to the function invocation.
+      PARAMS = 'params'.freeze
+
+      # Return Value or container of objects
+      RETURN = 'return'.freeze
+
+
+      #
+      # The name of the object type.
+      #
+      def self.typename
+        return KEYWORD
+      end
+
+      #
+      # The short name of the object type.
+      #
+      def self.short_typename
+        return KEYWORD_SHORT
+      end
+
+      #
+      # Set the value with any necessary type conversions.
+      #
+      def set_value( new_value )
+        self.value = new_value.to_s
+      end
+
+      #
+      # Set the value as an array.
+      #
+      def set_array_value( arr )
+        self.value = arr
+      end
+
+      #
+      # Does this object support multi-line values?
+      # Initially only true for scripts.
+      #
+      def multiline_value?
+        return true
+      end
+
+      # 
+      # Get the return value or container of objects.
+      # 
+      def return_any
+        return_any = find_child RETURN
+
+        # TODO: what does it look like to return objects?
+        # if return_any is a container with children, return the container
+
+        return return_any ? return_any.value : nil
+      end
+
+
+      # ---------------------------------------------------------------------
+      #    Children
+      # ---------------------------------------------------------------------
+
+      #
+      # Does this object have children to add when an object
+      # is created in interactive mode?
+      # This does not apply during obj load, etc.
+      #
+      def add_children_on_create?
+        return true
+      end
+
+      #
+      # Add children to this object.
+      # This is used by containers to add children needed
+      # for default configurations.
+      #
+      def add_default_children
+        fac = @engine.factory
+
+        fac.create_can PARAMS, self
+        fac.create_script ON_INVOKE, '', self
+        fac.create_untyped RETURN, '', self
+      end
+
+
+      # ---------------------------------------------------------------------
+      #    Messages
+      # ---------------------------------------------------------------------
+
+      #
+      # Get a list of message names that this object receives.
+      #
+      def self.messages
+        return super + [ 'invoke' ]
+      end
+
+      #
+      # Send the object the invoke message.
+      # Invoke the functdion and return the result.
+      #
+      def msg_invoke
+        return invoke
+      end
+
+
+      # ---------------------------------------------------------------------
+      #    Events
+      # ---------------------------------------------------------------------
+
+      #
+      # Run the on invoke script if there is one.
+      #
+      def run_on_invoke
+        o = find_child ON_INVOKE
+        return unless o
+
+        Gloo::Exec::Dispatch.message( @engine, 'run', o )
+      end
+
+
+      # ---------------------------------------------------------------------
+      #    Messages
+      # ---------------------------------------------------------------------
+
+      # 
+      # Invoke the function, run the script and return the result.
+      # 
+      def invoke
+        @engine.log.debug "Invoking function: #{name}"
+        
+        # TODO: Populate the parameters
+        # How do they get set?
+
+        run_on_invoke
+
+        return_value = return_any
+        @engine.heap.it.set_to return_value
+        
+        return return_value
+      end
+
+    end
+  end
+end
---
+++ test/objs/basic/function_test.rb
@@ -0,0 +1,43 @@
+require 'test_helper'
+
+class FunctionTest < BaseEngineTest
+
+  def test_the_typename
+    assert_equal 'function', Gloo::Objs::Function.typename
+  end
+
+  def test_the_short_typename
+    assert_equal 'ƒ', Gloo::Objs::Function.short_typename
+  end
+
+  def test_find_type
+    assert @dic.find_obj( 'function' )
+    assert @dic.find_obj( 'FUNCTION' )
+    assert @dic.find_obj( 'ƒ' )
+  end
+
+  def test_messages
+    msgs = Gloo::Objs::Function.messages
+    assert msgs
+    assert msgs.include?( 'invoke' )
+  end
+
+  def test_adds_children_on_create
+    o = Gloo::Objs::Function.new @engine
+    assert o.add_children_on_create?
+  end
+
+  def test_creating_function_object
+    o = @engine.parser.parse_immediate "create f as ƒ"
+    o.run
+
+    func = @engine.heap.root.children.first
+    assert func
+    assert func.children.count.positive?
+
+    params = func.children.first
+    assert params
+    assert_equal 'params', params.name
+  end
+
+end
