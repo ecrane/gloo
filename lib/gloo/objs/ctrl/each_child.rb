45d5bc12c39d4767eb20ab5d14b2856d0b2f544b
Eric Crane <eric.crane@mac.com>
2025-02-09 13:53:31 -0500

    adds group by

--- lib/gloo/objs/ctrl/each_child.rb
+++ lib/gloo/objs/ctrl/each_child.rb
@@ -10,6 +10,11 @@ module Gloo
 
       CHILD = 'child'.freeze
       IN = 'IN'.freeze
+
+      GROUP_BY = 'group_by'.freeze
+      ON_GROUP_START = 'on_group_start'.freeze
+      ON_GROUP_END = 'on_group_end'.freeze
+
       
       # ---------------------------------------------------------------------
       #    Create Iterator
@@ -34,6 +39,61 @@ module Gloo
         return false
       end
 
+      
+      # ---------------------------------------------------------------------
+      #    Group By
+      # ---------------------------------------------------------------------
+      
+      # 
+      # If the iterator has a group by,
+      # then we need to group by that value.
+      # Otherwise, we just loop for each child.
+      #
+      def has_group_by?
+        child = @iterator_obj.find_child GROUP_BY
+        return false unless child
+
+        return true
+      end
+
+      # 
+      # Get the child that is the group by.
+      # Return nil if there is no group by.
+      #
+      def group_by_value obj_can
+        return nil unless obj_can
+
+        child = @iterator_obj.find_child GROUP_BY
+        return nil unless child
+
+        group_by_child_name = child.value
+
+        obj = obj_can.find_child( group_by_child_name )
+        return nil unless obj
+
+        return obj.value
+      end
+
+      #
+      # Run the on group start script.
+      #
+      def run_on_group_start
+        o = @iterator_obj.find_child ON_GROUP_START
+        return unless o
+
+        Gloo::Exec::Dispatch.message( @engine, 'run', o )
+      end
+
+      #
+      # Run the on group end script.
+      #
+      def run_on_group_end
+        o = @iterator_obj.find_child ON_GROUP_END
+        return unless o
+
+        Gloo::Exec::Dispatch.message( @engine, 'run', o )
+      end
+
 
       # ---------------------------------------------------------------------
       #    Iterate
@@ -46,10 +106,37 @@ module Gloo
         o = @iterator_obj.find_child IN
         return unless o
 
+        # Set up for optional groups.
+        group_mode = false
+        if has_group_by?
+          group_mode = true
+          last_group = nil
+          first_time = true
+        end
+
         o = Gloo::Objs::Alias.resolve_alias( @engine, o )
         o.children.each do |child|
+          if group_mode
+            group_value = group_by_value( child )
+            # puts "last group = #{last_group}, group_value = #{group_value}"
+            if last_group != group_value
+              last_group = group_value
+              # puts "New group: #{group_value}"
+              if first_time
+                first_time = false
+              else
+                run_on_group_end
+              end
+              run_on_group_start
+            end
+          end
+
           set_child child
           @iterator_obj.run_do
+        end
+
+        if group_mode && !first_time
+          run_on_group_end
         end
       end
 
--- test/objs/ctrl/each_child_test.rb
+++ test/objs/ctrl/each_child_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class EachChildTest < BaseEngineTest
 
-  def test_use_for
+  def create_for_each_child
     i = @engine.parser.parse_immediate 'create for as each'
     i.run
     i = @engine.parser.parse_immediate 'tell for.word to unload'
@@ -10,7 +10,11 @@ class EachChildTest < BaseEngineTest
     i = @engine.parser.parse_immediate 'create for.child as alias'
     i.run
 
-    obj = @engine.heap.root.children.first
+    return @engine.heap.root.children.first
+  end
+
+  def test_use_for
+    obj = create_for_each_child
     assert obj
 
     assert Gloo::Objs::EachChild.use_for?( obj )
@@ -18,6 +22,29 @@ class EachChildTest < BaseEngineTest
     refute Gloo::Objs::EachLine.use_for?( obj )
     refute Gloo::Objs::EachRepo.use_for?( obj )
     refute Gloo::Objs::EachWord.use_for?( obj )
+  end
+
+  def test_getting_group_by_value
+    obj = create_for_each_child
+    assert obj
+    
+    iterator = Gloo::Objs::EachChild.new( @engine, obj )
+    assert iterator
+
+    group_by_value = iterator.group_by_value(nil)
+    assert_nil group_by_value
+    refute iterator.has_group_by?
+
+    i = @engine.parser.parse_immediate 'create for.group_by as string'
+    i.run
+
+    group_by_value = iterator.group_by_value(nil)
+    refute group_by_value
+    assert iterator.has_group_by?
+
+    i = @engine.parser.parse_immediate 'put "wow" into for.group_by'
+    i.run
+    assert iterator.has_group_by?
   end
 
 end
