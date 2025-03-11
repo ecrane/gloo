c8d83f5b852973e6cd5e0463501c58a9b453bc03
Eric Crane <encrane@datacor.com>
2025-03-11 11:29:41 -0400

    Update string_to_date.rb

--- lib/gloo/convert/string_to_date.rb
+++ lib/gloo/convert/string_to_date.rb
@@ -13,6 +13,11 @@ module Gloo
       # Convert the given string value to date.
       #
       def convert( value )
+        # 
+        # TODO: figure out why I needed to add this.
+        # The trailing ' am' was causing Chronic to fail.
+        # 
+        value = value[0..-3] if value.end_with?( ' am' )
         return Chronic.parse( value )
       end
 
