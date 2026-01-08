0ecf4ad6f44153b02550b371f33ab0251e617a0a
Eric Crane <encrane@datacor.com>
2026-01-07 19:42:18 -0500

    Update load.rb

--- lib/gloo/verbs/load.rb
+++ lib/gloo/verbs/load.rb
@@ -11,22 +11,42 @@ module Gloo
       KEYWORD = 'load'.freeze
       KEYWORD_SHORT = 'ld'.freeze
       MISSING_EXPR_ERR = 'Missing Expression!'.freeze
+      UNKNOWN_OPT_ERR = 'Unknown load option!'.freeze
+      WRONG_NUM_ARGS_ERR = 'Wrong number of arguments! 2 or 3 expected.'.freeze
+      FEATURE_NOT_IMPLEMENTED_ERR = 'Feature not implemented yet!'.freeze
+
+      FILE_OPT = 'file'.freeze
+      EXT_OPT = 'ext'.freeze
+      LIB_OPT = 'lib'.freeze
 
       #
       # Run the verb.
       #
       def run
-        fn = @tokens.second
-
-        if fn
-          @engine.log.debug "Getting ready to load file: #{fn}"
-          @engine.persist_man.load fn
+        if @tokens.token_count == 2
+          opt = FILE_OPT
+          fn = @tokens.second
+        elsif @tokens.token_count == 3
+          opt = @tokens.second.strip.downcase
+          fn = @tokens.last
         else
-          # @engine.err MISSING_EXPR_ERR
-          @engine.ext_manager.load_ext 'beep'
-          @engine.ext_manager.load_ext 'teams'
-          # @engine.ext_manager.load_ext 'test'
-          # @engine.ext_manager.load_ext 'alpha'
+          @engine.err WRONG_NUM_ARGS_ERR
+          return
+        end
+
+        if fn.blank?
+          @engine.err MISSING_EXPR_ERR
+          return
+        elsif opt == FILE_OPT
+          load_gloo_file fn
+        elsif opt == EXT_OPT
+          load_extension fn
+        elsif opt == LIB_OPT
+          # TODO: implement library loading
+          @engine.err FEATURE_NOT_IMPLEMENTED_ERR
+        else 
+          @engine.err UNKNOWN_OPT_ERR
+          return
         end
       end
 
@@ -42,6 +62,22 @@ module Gloo
       #
       def self.keyword_shortcut
         return KEYWORD_SHORT
+      end
+
+      # 
+      # Load a gloo file
+      # 
+      def load_gloo_file( fn )
+        @engine.log.debug "Getting ready to load gloo file: #{fn}"
+        @engine.persist_man.load fn
+      end
+
+      # 
+      # Load an extension
+      # 
+      def load_extension( name )
+        @engine.log.debug "Getting ready to load extension: #{name}"
+        @engine.ext_manager.load_ext name
       end
 
     end
