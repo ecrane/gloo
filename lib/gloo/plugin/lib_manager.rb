87ffe7b6e10721f8a0ffc232fa8137551b53306b
Eric Crane <encrane@datacor.com>
2026-01-24 11:24:16 -0500

    basic core lib manager working

--- lib/gloo/plugin/lib_manager.rb
+++ lib/gloo/plugin/lib_manager.rb
@@ -10,7 +10,8 @@ module Gloo
     class LibManager
 
       # Constants for library management
-      LIB_FILE = '_lib.rb'
+      GEM_PREFIX = 'gloo-'
+      INIT_CLASS_SUFFIX = 'Init'
 
       #
       # Set up the library manager.
@@ -29,23 +30,17 @@ module Gloo
       end
 
       # 
-      # Get the start file for the library.
+      # Get the start file for the gem core library.
       # 
-      def lib_start_file name
-        root = @engine.settings.lib_path
+      def core_lib_name name
+        return GEM_PREFIX + name
+      end
 
-        unless File.exist?( root )
-          @engine.log.error "Library directory does not exist: #{root}"
-          return nil
-        end
-        
-        f = File.join( root, name, name + LIB_FILE )
-        unless File.exist?( f )
-          @engine.log.error "Library start file does not exist: #{f}"
-          return nil
-        end
-        
-        return f
+      # 
+      # Get the library initialization class name.
+      # 
+      def init_class_name name
+        return name.camelize + INIT_CLASS_SUFFIX
       end
 
       # 
@@ -54,34 +49,45 @@ module Gloo
       # within the gloo libraries directory.
       #
       def load_lib( name )
-        @engine.log.debug "Loading library: #{name}"
-        fn = lib_start_file name
-
-        unless fn
-          @engine.log.error "Library start file not found for: #{name}"
-          return
-        end
+        @engine.log.debug "Loading core library: #{name}"
+        gem_name = core_lib_name name
         
-        @libraries[name] = fn
-        register_library name, fn
-        @engine.log.debug "Library loaded: #{name}"
+        @libraries[name] = gem_name
+        register_library name, gem_name
+        @engine.log.debug "Core library loaded: #{name}"
+      end
+
+      # 
+      # Require a gem by name, installing it if it's not available.
+      #
+      def require_gem gem_name
+        puts "Going to require gem: #{gem_name}"
+        begin
+          gem gem_name
+        rescue Gem::LoadError
+          system("gem install #{gem_name}")
+          Gem.clear_paths
+          gem gem_name
+        end
+
+        require gem_name
       end
 
       # 
       # Give the library a chance to register its verbs and objects.
       # 
-      def register_library( name, full_path )
-        require full_path
+      def register_library( name, gem_name )
+        require_gem gem_name
 
-        class_name = name.capitalize + "Lib"
-        @engine.log.debug "Looking for library to register: #{class_name}"
+        class_name = init_class_name name
+        @engine.log.debug "Looking for library entry point class to register: #{class_name}"
         begin
           plugin_class = Object.const_get( class_name )
           inst = plugin_class.new
           lib_cb = Callback.new( @engine )
           inst.register( lib_cb )
         rescue NameError
-          @engine.log.error "Warning: Could not find class #{class_name} in file #{full_path}"
+          @engine.log.error "Warning: Could not find class #{class_name}"
         end
       end
 
